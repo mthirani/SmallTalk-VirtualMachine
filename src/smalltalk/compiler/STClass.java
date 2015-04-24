6e5f7509844e959335e338ec84cd192d48ed3e75
Terence Parr <parrt@antlr.org>
2015-04-24 15:47:26 -0700

    provide more of STClass

--- src/smalltalk/compiler/STClass.java
+++ src/smalltalk/compiler/STClass.java
@@ -2,11 +2,26 @@ package smalltalk.compiler;
 
 import org.antlr.symtab.ClassSymbol;
 
-/**
- * Created by parrt on 4/24/15.
- */
 public class STClass extends ClassSymbol {
-	public STClass(String name) {
+	public final STSymbolTable symtab; // to (lazily) look up superclasses
+
+	public STClass(String name, String superClassName, STSymbolTable symtab) {
 		super(name);
+		setSuperClass(superClassName);
+		this.symtab = symtab;
+	}
+
+	public int getFieldIndex(String name) {
+		Symbol sym = resolve(name);
+		return sym!=null && sym.getScope() instanceof STClass ? sym.getInsertionOrderNumber() : -1;
+	}
+
+	public STMethod resolveMethod(String name) {
+		return (STMethod)super.resolveMethod(name);
+	}
+
+	@Override
+	public String toString() {
+		return "class "+name;
 	}
 }
